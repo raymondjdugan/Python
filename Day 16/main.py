03344db334cf97368886d3fc7c8742ba3d523ed4
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-07-13 12:23:53 -0500

    completed the OOP version of the coffee machine

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,1 +1,23 @@
-import turtle
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    order = input(print("What would you like? (espresso/latte/cappuccino):")).lower()
+    if order == "off":
+        is_on = False
+    elif order == "report":
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_machine.is_resource_sufficient(drink):
+            print(f"The cost of the drink is {drink.cost}")
+            if money_machine.make_payment(drink.cost):
+                coffee_machine.make_coffee(drink)
