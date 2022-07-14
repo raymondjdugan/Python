eef4021c18264f5e8347768542e6af5a7c6b479d
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-07-13 21:43:31 -0500

    compoleted quiz challenge with new data

--- Day 17/main.py
+++ Day 17/main.py
@@ -3,14 +3,14 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
-for questions in question_data:
-    new_question = Question(questions["text"], questions["answer"])
+for questions in question_data["results"]:
+    new_question = Question(questions["question"], questions["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
-    
+
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
