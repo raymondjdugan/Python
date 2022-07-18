8f6bb621cbe6497755320c6245c085f4a1b6ef73
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-07-18 10:59:17 -0500

    created dashed line

--- Day 18/main.py
+++ Day 18/main.py
@@ -3,12 +3,18 @@ import turtle
 
 timmy_the_turtle = t()
 
-timmy_the_turtle.pen(fillcolor="black", pencolor="red", pensize=5)
+# timmy_the_turtle.pen(fillcolor="black", pencolor="red", pensize=5)
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(90)
 
+for i in range(15):
+    timmy_the_turtle.forward(10)
+    if i % 2 == 0:
+        timmy_the_turtle.up()
+    else:
+        timmy_the_turtle.down()
 
 screen = s()
 screen.exitonclick()
