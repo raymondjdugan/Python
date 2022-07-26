3560bf38fdfee8004b0e31578920a879fdcac4da
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-07-26 10:55:36 -0500

    created scoreboard class

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Raymond's Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -16,14 +20,15 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
-
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
