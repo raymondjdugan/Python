da8fa42fc151ce04c51cd279f643ae4b89af9655
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-08-22 11:19:40 -0500

    Created scoreboard class and the init function

--- Turtle Crossing Game/main.py
+++ Turtle Crossing Game/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.up, "Up")
