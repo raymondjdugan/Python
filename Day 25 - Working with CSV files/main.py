bf143d2d469a2577fbf024702dabb80c8fa1a794
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-09-02 11:17:01 -0500

    Used Pandas' library to import, get column data, row data and export to new csv

--- Day 25 - Working with CSV files/main.py
+++ Day 25 - Working with CSV files/main.py
@@ -13,5 +13,28 @@
 #     print(temperatures)
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+
+# Get column data
+# print(f"The average temp for this list is {data.temp.mean()}")
+# print(f"the max value in the list is {data['temp'].max()}")
+
+# Get data in row
+# print(data[data.temp == data.temp.max()])
+# monday = data[data.day == "Monday"]
+# print((monday.temp * 9/5) + 32)
+
+# Create Dataframe From Scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
