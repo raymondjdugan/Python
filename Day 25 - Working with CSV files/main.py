59b805243ac71dfef2a0a4e0f4d3dd958cbddab9
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-09-02 10:48:14 -0500

    Installed Pandas library and imported csv file through it

--- Day 25 - Working with CSV files/main.py
+++ Day 25 - Working with CSV files/main.py
@@ -1,13 +1,17 @@
 # with open("weather_data.csv") as data_file:
 #     data = data_file.readlines()
 #     print(data)
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
