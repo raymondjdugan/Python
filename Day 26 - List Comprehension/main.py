ec45b53f76ac6037604b0fa066ec9843f94363c8
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-09-16 09:30:31 -0500

    Finished dictionary comprehension lecture

--- Day 26 - List Comprehension/main.py
+++ Day 26 - List Comprehension/main.py
@@ -17,28 +17,40 @@
 # upper_names = [name.upper() for name in names if len(name) > 5]
 # print(upper_names)
 
-
-# Square Numbers Challenge
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
-
-#Write your 1 line code 👇 below:
-squared_numbers = [num**2 for num in numbers]
-
-
-
-#Write your code 👆 above:
-
-print(squared_numbers)
-
-# Challenge 3
-# with open("file1.txt") as file1:
-#     file_one_nums = file1.readlines()
-# with open("file2.txt") as file2:
-#     file_two_nums = file2.readlines()
 #
-# result = [int(num) for num in file_one_nums if num in file_two_nums]
+# # Square Numbers Challenge
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # 🚨 Do Not Change the code above 👆
 #
-# # Write your code above 👆
+# #Write your 1 line code 👇 below:
+# squared_numbers = [num**2 for num in numbers]
+
+
+# #Write your code 👆 above:
 #
-# print(result)
+# print(squared_numbers)
+#
+# # Challenge 3
+# # with open("file1.txt") as file1:
+# #     file_one_nums = file1.readlines()
+# # with open("file2.txt") as file2:
+# #     file_two_nums = file2.readlines()
+# #
+# # result = [int(num) for num in file_one_nums if num in file_two_nums]
+# #
+# # # Write your code above 👆
+# #
+# # print(result)
+
+
+# Dictionary Comprehension
+# new_dict = {new_key:new_value for (key,value) in dict.items()}
+import random
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+scores = {student: random.randint(1, 100) for student in names}
+print(scores)
+
+passed_students = {student: score for (student, score) in scores.items() if score >= 60}
+print(passed_students)
+
